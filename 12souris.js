189a16ff24910205a3f22d7270f0e0d5a0f33bfc
simo sidox <160350557+sidox15@users.noreply.github.com>
2025-01-25 01:39:19 +0100

    Update 12souris.js

--- 12souris.js
+++ 12souris.js
@@ -4,17 +4,4 @@
     };
     document.addEventListener("contextmenu", function(e) {
         e.preventDefault();
-    });window.onscroll = function() {
-  const button = document.getElementById("sidoxtop");
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) { 
-    button.style.display = "block";
-  } else {
-    button.style.display = "none";
-  }
-};document.getElementById("sidoxtop").addEventListener("click", function() {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-});
-
+    });
