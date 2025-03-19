39a10025cb50eb579777e02d0dc7828c161eced1
simo sidox <160350557+sidox15@users.noreply.github.com>
2025-03-19 20:52:57 +0000

    Delete foter+pro-titel.js

--- foter+pro-titel.js
+++
@@ -1,9 +0,0 @@
-var elementsToHide = document.getElementsByClassName("footer");
-for (var i = 0; i < elementsToHide.length; i++) {
-    elementsToHide[i].style.display = "none";
-}
-
-var elementsToHide2 = document.getElementsByClassName("product-actions");
-for (var j = 0; j < elementsToHide2.length; j++) {
-    elementsToHide2[j].style.display = "none";
-}
